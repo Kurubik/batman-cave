2b0be7a6a02a33e3e57e98953a8f1fe2127bcad0
kurubik <benzins@gmail.com>
2017-09-05 10:30:38 +0300

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,45 @@
-const webpack = require('webpack');
-const path = require('path');
+module.exports.getConfig = function(type) {
+  var isDev = type === 'development';
 
-module.exports = {
-  entry: [
-    './src/index'
-  ],
-  module: {
-    loaders: [
-      { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.s?css$/, loader: 'style-loader!css-loader!sass-loader' },
-    ]
-  },
-  resolve: {
-    extensions: ['.js','.scss']
-  },
-  output: {
-    path: path.join(__dirname, '/dist'),
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  devtool: 'cheap-eval-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  },
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  ]
+  if (!isDev) {
+      const webpack = require('webpack');
+      const path = require('path');
+  }
+
+  var config = {
+    entry: [
+        isDev ? './app/scripts/main.js' : './app/scripts/main.js',
+    ],
+    module: {
+      loaders: [
+        { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel', query: { presets: ['react', 'es2015'] } }
+      ]
+    },
+    resolve: {
+      extensions: ['.jsx','.js', '.scss', '.styl']
+    },
+    output: {
+      path: isDev ? __dirname : path.join(__dirname, '/dist'),
+      publicPath: '/',
+      filename: 'main.js'
+    },
+    devtool: 'cheap-eval-source-map',
+    devServer: {
+      contentBase: './dist',
+      hot: true
+    },
+    plugins: {}
+  };
+
+    if(isDev){
+        config.devtool = 'eval';
+    } else {
+        config.plugins = [
+            new webpack.optimize.OccurrenceOrderPlugin(),
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NoEmitOnErrorsPlugin()
+        ]
+    }
+
+    return config;
 };
