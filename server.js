684baa6cd4368ac6fec361ddbb3d9c5ed57b6d1c
kurubik <benzins@gmail.com>
2017-09-08 17:45:35 +0300

    mongodb

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+
+var db = require('./db');
+
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/', function(request, response) {
+app.get('*', function(request, response) {
   response.sendFile(__dirname + '/dist/index.html');
 });
 
-app.listen(PORT, error => {
-  error
-  ? console.error(error)
-  : console.info(`Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`)
+db.connect('mongodb://batman:noname@ds125994.mlab.com:25994/batman-cave', function(err) {
+    if (err) {
+        console.log('Unable to connect to Mongo.');
+        process.exit(1);
+    } else {
+        app.listen(PORT, error => {
+            error
+                ? console.error(error)
+                : console.info(`Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`)
+        });
+    }
 });
